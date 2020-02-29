d13ad63ab89bed917528ce2c0f077958e79e6a6e
skippy <rcthedev@gmail.com>
2020-02-28 21:28:02 -0700

    added database

--- The-Perfect-Date/server.js
+++ The-Perfect-Date/server.js
@@ -9,7 +9,7 @@ let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Sage43236984C!",
     database: "user_profile_db"
   });
   // *****************************************************************************
