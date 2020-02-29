2828ed6b07d6848702309b2da466296e71c23a57
Jacqueline Tessmer <jntessmer@gmail.com>
2020-02-29 12:53:55 -0700

    thanks to eric

--- The-Perfect-Date/server.js
+++ The-Perfect-Date/server.js
@@ -23,6 +23,65 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Static directory
 app.use(express.static("public"));
+
+
+function populatedb() {
+  db.Question.create({
+    text: "favorite color?",
+    choice1: "pink",
+    choice2: "blue",
+    choice3: "red",
+    choice4: "green",
+    choice5: "orange",
+    choice6: "purple",
+    question_type: "bullshit"
+  }).then(function(err,data){
+    if (err) return console.log(err)
+  })
+  db.Question.create({
+    text: "favorite food?",
+    choice1: "none",
+    choice2: "all",
+    choice3: "people",
+    choice4: "cheese",
+    choice5: "meat",
+    choice6: "veggies",
+    question_type: "bullshit"
+  }).then(function(err,data){
+    if (err) return console.log(err)
+  })
+}
+
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/public/Main.html")
+});
+
+app.get("/api/questions",function(req,res){
+  db.Question.findAll()
+  .then(function(data){
+    console.log(data);
+    res.json(data)
+  })
+})
+
+app.post("/api/question",function(req,res){
+  for (let i = 0; i < req.body.choices.length; i++) {
+    db.UserResponse.create({
+      QuestionId: req.body.choices[i].id,
+      answer: req.body.choices[i].choice
+    })
+    .then(function(data){
+      console.log(data);
+      res.end()
+    })
+  console.log(req.body);
+  } 
+
+
+})
+
+
 // Routes
 // =============================================================
 // require("./routes/html-routes.js")(app);
@@ -32,6 +91,7 @@ app.use(express.static("public"));
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
+    populatedb();
     console.log("App listening on PORT " + PORT);
   });
 });
